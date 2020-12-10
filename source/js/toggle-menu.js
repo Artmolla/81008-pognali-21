397abab2d9dd56525b9f2490ea1eaf5498d24c79
Artem <artmolla@gmail.com>
2020-12-10 14:55:44 +0200

    Внес правки по рекомендациям наставника

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -1,10 +1,13 @@
 let openMenuButton = document.querySelector('.page-header__toggle-menu-button');
 let menuBody = document.querySelector('.menu__body');
 let pageHeader = document.querySelector('.page-header');
+let authorizationButton = document.querySelector('.authorization');
+console.log(authorizationButton);
 
 openMenuButton.addEventListener('click', function(evt){
   evt.preventDefault();
   menuBody.classList.toggle('menu__body--menu-open');
   openMenuButton.classList.toggle('page-header__toggle-menu-button--menu-open');
   pageHeader.classList.toggle('page-header--menu-open');
+  authorizationButton.classList.toggle('authorization--menu-open');
 });
