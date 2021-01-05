fa0f0178b211e81db8837b370db727a2a22ee8a1
Artem <artmolla@gmail.com>
2021-01-05 22:23:22 +0200

    Поправил расчет окружности для индикатора уровня пользователя

--- source/js/user-score-progress-bar.js
+++ source/js/user-score-progress-bar.js
@@ -1,7 +1,7 @@
 let circles = document.getElementsByClassName('user-score__progress-ring__circle');
 let usersScore = document.getElementsByClassName('user-score__score');
 const radius = circles[0].r.baseVal.value;
-const circumference = radius * 2 * Math.PI;
+const circumference = radius * 2 * Math.PI - 4;
 
 
 for (let i = 0; i < circles.length; i++) {
