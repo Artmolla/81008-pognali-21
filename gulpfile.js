e3ab794f4fff0c31f5ccdbcbe9bd403b63fab021
Artem <artmolla@gmail.com>
2021-01-08 22:18:48 +0200

    Удалил создание  source.map для build версии сборки

--- gulpfile.js
+++ gulpfile.js
@@ -22,15 +22,14 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(gulp.dest("source/css"))
-
     .pipe(postcss([
       autoprefixer(),
       csso()
     ]))
     .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
-    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
